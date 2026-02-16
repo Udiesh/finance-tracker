f9dc970db9add158e2f2b9e788dc12a5e5ec3c54
Your Name <udieshkumar9@gmail.com>
2026-02-16 21:32:27 +0530

    fixes to the alembix scripts

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,6 +1,11 @@
-from logging.config import fileConfig
+import os
+from dotenv import load_dotenv, load_dotnev
 
-from sqlalchemy import engine_from_config
+load_dotenv()
+
+from logging.config import fileConfig
+from app.config import DATABASE_URL
+from sqlalchemy import engine_from_config, create_engine
 from sqlalchemy import pool
 
 from alembic import context
@@ -8,6 +13,9 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+#override alembic.ini with your .env value
+config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -61,11 +69,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable =create_engine(DATABASE_URL)
 
     with connectable.connect() as connection:
         context.configure(
